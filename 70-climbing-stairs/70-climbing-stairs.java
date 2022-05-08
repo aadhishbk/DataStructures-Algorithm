3f1f5746b6513eb0df786b22fd5e6049003a84d7
Aadhish B K <86835219+aadhishbk@users.noreply.github.com>
2022-05-08 13:24:39 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (89.38%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,10 +1,21 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[] = new int[n+1];
+        /*int dp[] = new int[n+1];
         dp[0] = 1;dp[1]=1;
         for(int i=2;i<=n;i++){
             dp[i] = dp[i-2]+dp[i-1];
         }
         return dp[n];
+        */
+        
+        //it resembles fibonacci number problem
+        
+        int prev=1,pprev =1;
+        for(int i=2;i<=n;i++){
+            int curr = pprev+prev;
+            pprev = prev;
+            prev =curr;
+        }
+        return prev;
     }
 }

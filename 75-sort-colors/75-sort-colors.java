1e5ecdc55ea79cfd7ff16b92bf67358939fca19e
Aadhish B K <86835219+aadhishbk@users.noreply.github.com>
2022-05-08 15:37:01 +0530

    Time: 1 ms (33.54%), Space: 43 MB (9.22%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void sortColors(int[] arr) {
+        int n = arr.length;
+        for(int i=0;i<n; i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i] >arr[j])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+                
+            }
+        }
+    }
+}

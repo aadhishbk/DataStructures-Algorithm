24a67b1fa39ecac294d6c512646f5de04102b5a4
Aadhish B K <86835219+aadhishbk@users.noreply.github.com>
2022-05-11 23:15:02 +0530

    Time: 4 ms (15.89%), Space: 54.5 MB (14.40%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -3,17 +3,33 @@ class Solution {
         Set<Integer> rlist = new HashSet<>();
         Set<Integer> clist = new HashSet<>();
 
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[i].length; j++){
-                if(matrix[i][j] == 0){
+        for(int i=0; i<matrix.length; i++)
+        {
+            for(int j=0; j<matrix[i].length; j++)
+            {
+                if(matrix[i][j] == 0)
+                {
                     rlist.add(i);
                     clist.add(j);
                 }
             }
         }
         
-        Iterator it = rlist.iterator();
-        while(it.hasNext()){
+        for(int i=0; i< matrix.length;i++){
+             for(int j=0; j<matrix[i].length; j++)
+            {
+            
+                 if(rlist.contains(i) || clist.contains(j)){
+                     matrix[i][j]=0;
+                 }
+             }
+            
+        }
+        
+        
+       /* Iterator it = rlist.iterator();
+        while(it.hasNext())
+        {
             int ir= (Integer)it.next();
             for(int j=0;j<matrix[ir].length; j++)
             {
@@ -22,17 +38,15 @@ class Solution {
         }
         
         Iterator itc = clist.iterator();
-        while(itc.hasNext()){
+        while(itc.hasNext())
+        {
             int j =(Integer) itc.next();
-            for(int i=0; i<matrix.length; i++){
-                   
+            for(int i=0; i<matrix.length; i++)
+            {
                    matrix[i][j]=0;
-                   
-               }
             }
-            
-        
-        
-        
+        }
+        */
+ 
     }
 }

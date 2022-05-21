d25c6ab36fece7695611c75394aadd3ef2ebed15
Aadhish B K <86835219+aadhishbk@users.noreply.github.com>
2022-05-21 07:18:10 +0530

    Time: 3 ms (8.02%), Space: 41.9 MB (26.10%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.java
+++ 226-invert-binary-tree/226-invert-binary-tree.java
@@ -38,4 +38,62 @@ class Solution {
         return list;
         
     }
+    
+    /*
+    public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        
+        if (root == null) {
+            return null;
+        }
+
+        final Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        
+        while(!stack.isEmpty()) {
+            final TreeNode node = stack.pop();
+            final TreeNode left = node.left;
+            node.left = node.right;
+            node.right = left;
+            
+            if(node.left != null) {
+                stack.push(node.left);
+            }
+            if(node.right != null) {
+                stack.push(node.right);
+            }
+        }
+        return root;
+    }
+}
+
+
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        
+        if (root == null) {
+            return null;
+        }
+
+        final Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()) {
+            final TreeNode node = queue.poll();
+            final TreeNode left = node.left;
+            node.left = node.right;
+            node.right = left;
+
+            if(node.left != null) {
+                queue.offer(node.left);
+            }
+            if(node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        return root;
+    }
+    
+}
+*/
 }
